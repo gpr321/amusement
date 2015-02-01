ba7d24589d836e52413517e433fb889a49ce278d
gpr321 <595665831@qq.com>
2015-02-02 00:14:11 +0800

    修改了 NSObject + GP 头文件

--- exploreInRuntime/exploreInRuntime/Catogory/NSObject+GP.h
+++ exploreInRuntime/exploreInRuntime/Catogory/NSObject+GP.h
@@ -2,20 +2,17 @@
 //  NSObject+GP.h
 //  exploreInRuntime
 //
-//  Created by mac on 15-1-30.
+//  Created by mac on 15-2-2.
 //  Copyright (c) 2015年 gpr. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#ifndef exploreInRuntime_NSObject_GP_h
+#define exploreInRuntime_NSObject_GP_h
+
 #import "NSObject+Invocation.h"
 #import "NSObject+Notification.h"
 #import "NSObject+Method.h"
 #import "NSObject+SetClass.h"
 #import "NSObject+Property.h"
 #import "NSObject+Invocation.h"
-
-@interface NSObject (GP)
-
-
-
-@end
+#endif
