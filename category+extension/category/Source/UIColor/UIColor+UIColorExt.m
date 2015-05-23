259b72d78f1b0d332a0f616e5d0f315c3ad2a7d4
gaopeirong <gaopeirong@cloudfocus.cn>
2015-05-23 12:23:43 +0800

    ad

---
+++ category+extension/category/Source/UIColor/UIColor+UIColorExt.m
@@ -0,0 +1,82 @@
+//
+//  UIColor+UIColorExt.m
+//  doctorApp
+//
+//  Created by richardYang on 3/23/14.
+//  Copyright (c) 2014 richardYang. All rights reserved.
+//
+
+#import "UIColor+UIColorExt.h"
+
+@implementation UIColor (UIColorExt)
++ (UIColor *)colorFromRGB:(NSInteger)rgbValue withAlpha:(CGFloat)alpha  {
+    return [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16)) / 255.0
+                           green:((float)((rgbValue & 0xFF00) >> 8)) / 255.0
+                            blue:((float)(rgbValue & 0xFF)) / 255.0 alpha:alpha];
+    
+}
+
++ (UIColor *)colorFromRGB:(NSInteger)rgbValue {
+    return [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16)) / 255.0
+                           green:((float)((rgbValue & 0xFF00) >> 8)) / 255.0
+                            blue:((float)(rgbValue & 0xFF)) / 255.0 alpha:1.0];
+    
+}
+
+/**
+ * hexString eg. #ff0000
+ */
++ (UIColor *)colorWithHexString:(NSString *)hexString {
+    return [UIColor colorWithHexString:hexString alpha:1.0];
+}
+
+/**
+ * hexString eg. #ffffffff
+ */
++ (UIColor *)colorWithHexStringWithAlpha:(NSString*)hexString {
+   	if ([hexString hasPrefix:@"0x"] || [hexString hasPrefix:@"0X"]) {
+        hexString = [hexString substringFromIndex:2];
+	} else if ([hexString hasPrefix:@"#"]) {
+        hexString = [hexString substringFromIndex:1];
+    }
+    
+    unsigned int value = 0;
+    BOOL flag = [[NSScanner scannerWithString:hexString] scanHexInt:&value];
+    if(NO == flag)
+        return [UIColor clearColor];
+    float r, g, b, a;
+    a = (value & 0x000000FF) / 255.0;
+    value = value >> 8;
+    b = value & 0x000000FF;
+    value = value >> 8;
+    g = value & 0x000000FF;
+    value = value >> 8;
+    r = value;
+    
+    return [UIColor colorWithRed:r/255.0f green:g/255.0f blue:b/255.0f alpha:a];
+}
+
++ (UIColor *)colorWithHexString:(NSString *)hexString alpha:(CGFloat)alpha {
+	if ([hexString hasPrefix:@"0x"] || [hexString hasPrefix:@"0X"]) {
+        hexString = [hexString substringFromIndex:2];
+	} else if ([hexString hasPrefix:@"#"]) {
+        hexString = [hexString substringFromIndex:1];
+    }
+    
+    unsigned int value = 0;
+    BOOL flag = [[NSScanner scannerWithString:hexString] scanHexInt:&value];
+    if(NO == flag)
+        return [UIColor clearColor];
+    float r, g, b, a;
+    a = alpha;
+    b = value & 0x0000FF;
+    value = value >> 8;
+    g = value & 0x0000FF;
+    value = value >> 8;
+    r = value;
+    
+    return [UIColor colorWithRed:r/255.0f green:g/255.0f blue:b/255.0f alpha:a];
+}
+
+
+@end
